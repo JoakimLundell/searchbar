7b15927a67bbc5ef9c4eaaed9239d6175ac4e276
Joakim Lundell <joakim.lundell@photowall.se>
2016-03-11 16:11:29 +0100

    add files

---
+++ js/searchbar.js
@@ -0,0 +1,11 @@
+(function ($) {
+  console.log("loaded ok");
+  //var searchbarIcon = $("#searchbar-icon");
+  //var searchbar = $("#searchbar");
+  //var searchbarInput = $("#searchbar-input");
+
+  $( "#target" ).click(function() {
+    alert( "Handler for .click() called." );
+  });
+
+  })(jQuery);
